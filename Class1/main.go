68bf52a84d929d2e0807e815f333b5f634ba2d0c
Martinsr99 <silesreche.martin@gmail.com>
2024-03-12 23:57:26 +0100

    Add clousure function

--- Class1/main.go
+++ Class1/main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	Tabla := tabla(2)
+	for i:= 1; i<= 10; i++ {
+		fmt.Printf("2 x %v = %v \n",i,Tabla())
+	}
+}
+
+// Clousure
+func tabla(valor int) func() int {
+	numero := valor
+	secuencia:= 0
+	return func() int {
+		secuencia ++
+		return numero*secuencia
+	}
 }
