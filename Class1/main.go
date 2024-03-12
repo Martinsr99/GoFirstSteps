ec2b017852dbb952ff9b6a12d8dc992521efe23d
Martinsr99 <silesreche.martin@gmail.com>
2024-03-13 00:35:41 +0100

    feat: Add nested structure

--- Class1/main.go
+++ Class1/main.go
@@ -1,20 +1,41 @@
-package main 
+package main
 
 import "fmt"
 
 func main() {
-	Tabla := tabla(2)
-	for i:= 1; i<= 10; i++ {
-		fmt.Printf("2 x %v = %v \n",i,Tabla())
-	}
+
+	//Clousure
+
+	// Tabla := tabla(2)
+	// for i:= 1; i<= 10; i++ {
+	// 	fmt.Printf("2 x %v = %v \n",i,Tabla())
+	// }
+
+	// Estructura anidada
+	categoria := Categoria{Id: 1, Nombre: "Categoria 1", Slug: "categoria-1"}
+	producto := Producto{Id: 1, Nombre: "Mesa de ordenador", Slug:"mesa-de-ordenador",Precio:1234,CategoriaId: categoria}
+	fmt.Printf("%+v \n",producto)
 }
 
 // Clousure
-func tabla(valor int) func() int {
-	numero := valor
-	secuencia:= 0
-	return func() int {
-		secuencia ++
-		return numero*secuencia
-	}
+// func tabla(valor int) func() int {
+// 	numero := valor
+// 	secuencia := 0
+// 	return func() int {
+// 		secuencia++
+// 		return numero * secuencia
+// 	}
+// }
+
+type Categoria struct {
+	Id     int
+	Nombre string
+	Slug   string
+}
+type Producto struct {
+	Id          int
+	Nombre      string
+	Slug        string
+	Precio      int
+	CategoriaId Categoria
 }
